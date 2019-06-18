5b3d55046589cf96b93fa1616e9476c8b6bce820
HoangNguyen17193 <hoangnh17193@gmail.com>
2019-06-18 23:50:02 +0700

    add request model

---
+++ src/models/request.ts
@@ -0,0 +1,44 @@
+import { throws } from "assert";
+
+export default class Request {
+  private _url: string;
+  private _type: string;
+  private _headers: string;
+  private _body: string;
+  private _form: string;
+  private _result: any;
+  constructor(url: string, type: string, headers: string, body: string, form: string) {
+    this._url = url;
+    this._type = type;
+    this._headers = headers;
+    this._body = body;
+    this._form = form;
+  }
+  get url() {
+    return this._url;
+  }
+
+  get type() {
+    return this._type;
+  }
+
+  get headers() {
+    return this._headers;
+  }
+
+  get body() {
+    return this._body;
+  }
+
+  get form() {
+    return this._form;
+  }
+
+  get result() {
+    return this._result;
+  }
+
+  set result(result: any) {
+    this._result = result;
+  }
+} 
