53b6ac300d22f90f9b2c382d638922d520fbf019
Hoang Nguyen <hoangnh17193@gmail.com>
2019-06-27 00:18:28 +0700

    refactor create request panel

---
+++ src/controllers/mainController.ts
@@ -0,0 +1,34 @@
+import Request from '../models/request';
+import RequestService from '../services/request';
+import HistoryService from '../services/history';
+import RequestPanel from '../views/requestPanel/RequestPanel';
+
+export default class BaseRunner {
+  public makeRequest(url: string, type: string, headers:string, body:string, form:string) {
+    const requestModel = new Request(url, type, headers, body, form);
+    HistoryService.write(requestModel);
+    return RequestService.request(requestModel);
+  }
+
+  public createRequestPanel(request: Request) {
+    const rapPanel = new RequestPanel(request);
+    const panel = rapPanel.create();
+    panel.webview.onDidReceiveMessage(async (message) => {
+      switch (message.command) {
+        case "request": {
+          const { url, type, headers, body, form } = message;
+          try {
+            const result = await this.makeRequest(url, type, headers, body, form);
+            const newRequest = new Request(url, type, headers, body, form);
+            newRequest.result = result || 'No Content';
+            rapPanel.reload(newRequest);
+          } catch (error) {
+            const newRequest = new Request(url, type, headers, body, form);
+            newRequest.error = error.response ? error.response : error;
+            rapPanel.reload(newRequest);
+          }
+        }
+      }
+    });
+  }
+}
