c6115a104c8fef0ef232802b24500ac9b1fbb947
HoangNguyen17193 <hoangnh17193@gmail.com>
2019-07-01 10:19:48 +0700

    refresh menu

--- src/controllers/mainController.ts
+++ src/controllers/mainController.ts
@@ -2,14 +2,19 @@ import Request from '../models/request';
 import RequestService from '../services/request';
 import HistoryService from '../services/history';
 import RequestPanel from '../views/requestPanel/RequestPanel';
+import TreeDataProvider from '../views/menu/TreeDataProvider';
 
 export default class BaseRunner {
+  private _menu:TreeDataProvider;
+
+  constructor(menu: TreeDataProvider) {
+    this._menu = menu;
+  }
   public makeRequest(name:string, url: string, type: string, headers:string, body:string, form:string) {
     const requestModel = new Request(name, url, type, headers, body, form);
     HistoryService.write(requestModel);
     return RequestService.request(requestModel);
   }
-
   public createRequestPanel(request: Request) {
     const rapPanel = new RequestPanel(request);
     const panel = rapPanel.create();
@@ -22,10 +27,12 @@ export default class BaseRunner {
             const newRequest = new Request(name, url, type, headers, body, form);
             newRequest.result = result || 'No Content';
             rapPanel.reload(newRequest);
+            this._menu.refresh();
           } catch (error) {
             const newRequest = new Request(name, url, type, headers, body, form);
             newRequest.error = error.response ? error.response : error;
             rapPanel.reload(newRequest);
+            this._menu.refresh();
           }
         }
       }
